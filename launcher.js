e715c45a6d7b8f1e7ed53874dab0f8561cc090bb
SarthakAgarwal383 <73892532+SarthakAgarwal383@users.noreply.github.com>
2021-01-14 19:06:23 +0530

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,53 @@
+
+    
+class slingshot{
+    constructor(bodyA,pointB){
+        var options={
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.4,
+            length: 10
+          
+        }
+        this.sling1= loadImage("sling1.png");
+        this.sling2= loadImage("sling2.png");
+        this.sling3= loadImage("sling3.png");
+    
+        this.sling= Matter.Constraint.create(options);
+        this.pointB= pointB;
+        World.add(world,this.sling);
+    
+    }
+    
+    display(){
+        image(this.sling1,200,340);
+        image(this.sling2,170,340);
+        if(this.sling.bodyA){
+    
+            var Pa= this.sling.bodyA.position;
+            var Pb= this.pointB;
+            push();
+            stroke(48,22,8);
+            if(Pa.x<250){
+            strokeWeight(7);
+            line(Pa.x-20,Pa.y,Pb.x-10,Pb.y);
+            line(Pa.x-20,Pa.y,Pb.x+30,Pb.y-10);
+            image(this.sling3,Pa.x-30,Pa.y-10,15,30);
+            }
+            else{
+                strokeWeight(4);
+            line(Pa.x+25,Pa.y,Pb.x-10,Pb.y);
+            line(Pa.x+25,Pa.y,Pb.x+30,Pb.y-10);
+            image(this.sling3,Pa.x+25,Pa.y-10,15,30);
+            }
+            pop();
+    
+        }
+    }
+     fly(){
+    this.sling.bodyA= null;
+    }
+     attach(body){
+         this.sling.bodyA= body;
+     }
+    }
